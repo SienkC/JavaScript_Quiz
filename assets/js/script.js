6437e46a96b8a1805a894be39512d0f8cc32d752
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-03 14:59:49 -0400

    Button hides main page when clicked and shows questions section

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@
 // show score and allow user to enter initials
 // save score and initials to local storage
 // score will show on score page
+
+// if start button pressed "display = none" for home page
+var startEl = document.querySelector('#start');
+var main = document.querySelector("#display_main_page");
+var questions = document.querySelector("#display_questions");
+
+startEl.addEventListener('click', function (event) {
+    event.preventDefault();
+    main.style.display = "none";
+    questions.style.display = "contents";
+    });
