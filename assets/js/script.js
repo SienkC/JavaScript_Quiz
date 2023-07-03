cb25d70e3c68e06903f992d6544c3545b80a9eae
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-03 19:47:03 -0400

    Added function for timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@
 // save score and initials to local storage
 // score will show on score page
 
+// Get elements from HTML
+var startEl = document.querySelector("#start");
+var main = document.querySelector("#display_main_page");
+var questions = document.querySelector("#display_questions");
+var timerEl = document.querySelector("#time");
+
 const qAndA = {
     q1: {
         question: "Commonly used data types DO NOT include:",
@@ -47,11 +53,8 @@ const qAndA = {
 // number of questions
 var numQuest = Object.keys(qAndA).length;
 
-// Get elements from HTML
-var startEl = document.querySelector('#start');
-var main = document.querySelector("#display_main_page");
-var questions = document.querySelector("#display_questions");
-
+// timer will count down from this number
+var timer = 75;
 
 startEl.addEventListener('click', function (event) {
     // if start button pressed home page is replaced with questions
@@ -60,54 +63,70 @@ startEl.addEventListener('click', function (event) {
     questions.style.display = "contents";
 
     // timer starts
+    timeLeft();
 
     // loop for questions
     var test = qAndA.q1;
     showQuestions(test);
 
     return;
-    });
+});
 
 
-    function showQuestions(questionNum) {
-        // array for answer choices
-        var choices = ["correct", "wrong1", "wrong2", "wrong3"];
+// timer counts down every second (1000ms)
+function timeLeft(){
+    var timerInt = setInterval(function() {
 
-        // randomize order of choices
-        choices = shuffleArray(choices);
+        timerEl.textContent = "Time: " + timer;
+        timer--;
 
-        // Show question
-        document.querySelector("#question").textContent = questionNum.question;
-
-        // loop to create answer choices
-        for(let i = 0; i < choices.length; i++){
-            var temp = document.createElement("li");
-            temp.textContent = questionNum[choices[i]];
-            document.querySelector("#answers").appendChild(temp);
-        }
-        
-        return;
-    }
-
-    // Based on Fisher-Yates shuffle algorithm
-    function shuffleArray(choices) {
-        var currentValue = choices.length;
-        var randomValue;
-        var temp;
-
-        // currentValue will get smaller until 0
-        while (currentValue != 0) {
-            // New index for current value
-            randomValue = Math.floor(Math.random() * currentValue);
-            currentValue--;
-
-            // Temp holds onto previous value, so it is not forgotten
-            temp = choices[currentValue];
-
-            // Swap items in both indexes
-            choices[currentValue] = choices[randomValue];
-            choices[randomValue] = temp;
+        // Stops timer when it reaches 0
+        if(timer === 0){
+            clearInterval(timerInt);
         }
 
-        return choices;
+    }, 1000)
+}
+
+function showQuestions(questionNum) {
+    // array for answer choices
+    var choices = ["correct", "wrong1", "wrong2", "wrong3"];
+
+    // randomize order of choices
+    choices = shuffleArray(choices);
+
+    // Show question
+    document.querySelector("#question").textContent = questionNum.question;
+
+    // loop to create answer choices
+    for(let i = 0; i < choices.length; i++){
+        var temp = document.createElement("li");
+        temp.textContent = questionNum[choices[i]];
+        document.querySelector("#answers").appendChild(temp);
     }
+    
+    return;
+}
+
+// Based on Fisher-Yates shuffle algorithm
+function shuffleArray(choices) {
+    var currentValue = choices.length;
+    var randomValue;
+    var temp;
+
+    // currentValue will get smaller until 0
+    while (currentValue != 0) {
+        // New index for current value
+        randomValue = Math.floor(Math.random() * currentValue);
+        currentValue--;
+
+        // Temp holds onto previous value, so it is not forgotten
+        temp = choices[currentValue];
+
+        // Swap items in both indexes
+        choices[currentValue] = choices[randomValue];
+        choices[randomValue] = temp;
+    }
+
+    return choices;
+}
