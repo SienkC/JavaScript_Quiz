1693f5a30bfc253ee8586852475adf95de4d1a86
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-03 18:33:30 -0400

    Added shuffleArray function and made note in README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaScript_Quiz
 Timed quiz on fundamentals of JavaScript
+
+<!-- Remember to credit Fisher-Yates method for shuffling array -->
--- assets/js/script.js
+++ assets/js/script.js
@@ -44,35 +44,68 @@ const qAndA = {
     }
 };
 
-// if start button pressed "display = none" for home page
+// number of questions
+var numQuest = Object.keys(qAndA).length;
+
+// Get elements from HTML
 var startEl = document.querySelector('#start');
 var main = document.querySelector("#display_main_page");
 var questions = document.querySelector("#display_questions");
 
+
 startEl.addEventListener('click', function (event) {
+    // if start button pressed home page is replaced with questions
     event.preventDefault();
     main.style.display = "none";
     questions.style.display = "contents";
 
     // timer starts
 
-    // show questions
-    showQuestions();
+    // loop for questions
+    var test = qAndA.q1;
+    showQuestions(test);
 
     return;
     });
 
 
-    function showQuestions() {
-        // var counter = 0;
+    function showQuestions(questionNum) {
+        // array for answer choices
+        var choices = ["correct", "wrong1", "wrong2", "wrong3"];
 
-        // while loop for # quests?
-        document.querySelector("#question").textContent = qAndA.q1.question;
+        // randomize order of choices
+        choices = shuffleArray(choices);
+
+
+        document.querySelector("#question").textContent = questionNum.question;
 
         // for loop that loops through questions?
         var temp = document.createElement("li");
-        temp.textContent = qAndA.q1.correct;
+        temp.textContent = questionNum.correct;
         document.querySelector("#answers").appendChild(temp);
 
         return;
+    }
+
+    // Based on Fisher-Yates shuffle algorithm
+    function shuffleArray(choices) {
+        var currentValue = choices.length;
+        var randomValue;
+        var temp;
+
+        // currentValue will get smaller until 0
+        while (currentValue != 0) {
+            // New index for current value
+            randomValue = Math.floor(Math.random() * currentValue);
+            currentValue--;
+
+            // Temp holds onto previous value, so it is not forgotten
+            temp = choices[currentValue];
+
+            // Swap items in both indexes
+            choices[currentValue] = choices[randomValue];
+            choices[randomValue] = temp;
+        }
+
+        return choices;
     }
