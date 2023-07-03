5c82bada48db8069134f7e31ecf914e4f4a56cd9
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-07-03 13:34:41 -0400

    Added basic layout to js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// if start button pressed "display = none" for home page
+// loop through questions 
+// move onto next question when answer chosen and subtract from time if wrong
+// stop loop when time = 0
+// show score and allow user to enter initials
+// save score and initials to local storage
+// score will show on score page
